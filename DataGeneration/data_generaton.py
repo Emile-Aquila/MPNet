d015c6cb6598d00927891a8fa463902a59592d8c
Emile-Aquila <tmk8oakan6130@gmail.com>
2023-01-20 18:46:22 +0900

    add data save function, and refactor

--- DataGeneration/data_generaton.py
+++ DataGeneration/data_generaton.py
@@ -8,24 +8,26 @@ from TrajectryGeneration.models import Robot_model
 from TrajectryGeneration.objects.field import Field, Rectangle, Circle
 from TrajectryGeneration.objects.Point2D import Point2D
 from TrajectryGeneration.RRT import RRT_star, RRT
+import yaml
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '/TrajectryGeneration'))
 
 
 def field_generator(N: int, object_num: int, point_num: int) -> (list[Field], list[list[Point2D]]):
     # N個のfieldをランダムに作成
-    L: float = 20.0
+    field_L: float = 20.0
+    obstacle_L: float = 5.0
     fields: list[None | Field] = [None] * N
     points: list[list[None | Point2D]] = [[None] * point_num for _ in range(N)]
-    obj_points = np.random.rand(N, object_num, 2) * L - L / 2.0
+    obj_points = np.random.rand(N, object_num, 2) * (field_L-obstacle_L) - (field_L - obstacle_L) / 2.0
 
     for i in range(N):
-        tmp_field = Field(w=L, h=L, center=True)
-        objects = [Rectangle(x=pt[0], y=pt[1], w=5.0, h=5.0, theta=0.0, fill=True) for pt in obj_points[i]]
+        tmp_field = Field(w=field_L, h=field_L, center=True)
+        objects = [Rectangle(x=pt[0], y=pt[1], w=obstacle_L, h=obstacle_L, theta=0.0, fill=True) for pt in obj_points[i]]
         tmp_field.obstacles += objects
         index: int = 0
         while index < point_num:
-            xy = np.random.rand(2) * L - L / 2.0
+            xy = np.random.rand(2) * field_L - field_L / 2.0
             if not tmp_field.check_collision(Circle(xy[0], xy[1], 0.1)):
                 points[i][index] = Point2D(xy[0], xy[1])
                 index += 1
@@ -48,7 +50,7 @@ def generate_point_cloud(field: Field, num_pc: int, object_num: int) -> np.array
     return point_clouds
 
 
-def gen_test_data(fields, points):
+def gen_path_data(fields: list[Field], points: list[list[Point2D]]) -> list[list[list[Point2D]]]:
     ans_paths = []
     for i in tqdm(range(len(fields))):
         rrt_star = RRT_star(fields[i], None, R=20.0, eps=0.25, goal_sample_rate=0.05, check_length=None)  # RRT*
@@ -65,15 +67,48 @@ def gen_test_data(fields, points):
     return ans_paths
 
 
+def import_training_data(path_data_path: str, pc_data_path: str) -> tuple[dict, dict]:
+    with open(path_data_path) as file:
+        path_data = yaml.load(file, yaml.Loader)
+    with open(pc_data_path) as file:
+        pc_data = yaml.load(file, yaml.Loader)
+    return path_data, pc_data
+
+
 if __name__ == '__main__':
-    fields, points = field_generator(2, 7, 5)
-    datas = gen_test_data(fields, points)
+    generate_field_num = 1  # フィールドの生成数
+    object_num = 1  # フィールド上のオブジェクト数
+    point_num = 2  # フィールド上に配置する点の数(ここからランダムにstart/goal地点が選択される。)
+    point_cloud_num = 100  # 点群の点数
+
+    fields, points = field_generator(generate_field_num, object_num, point_num)  # フィールド生成
+    paths = gen_path_data(fields, points)  # 各フィールドとスタート/ゴール地点に対して経路を生成。
+
+    id_pc_data = dict()  # (field_id, point cloud)のデータ
+    id_path_data = dict()  # (field_id, path)のデータ
+
+    for i, tmp_paths in enumerate(paths):
+        for tmp_path in tmp_paths:
+            tmp_path_tuple = [point.getXY() for point in tmp_path]
+            id_path_data[i] = tmp_path_tuple
 
     for i in range(len(fields)):
-        path = datas[i][0]
+        point_clouds = generate_point_cloud(fields[i], point_cloud_num, object_num)  # 各fieldに対応する点群データ
+        id_pc_data[i] = point_clouds
+
+    with open("./id_pc_data.yaml", "w") as file:
+        yaml.dump(id_pc_data, file)
+
+    with open("./id_path_data.yaml", "w") as file:
+        yaml.dump(id_path_data, file)
+
+    for i in range(len(fields)):
+        path = paths[i][0]
         ax = fields[i].plot_path_control_point(path, show=False)
-        pcs = generate_point_cloud(fields[i], 2800, 7)
+        pcs = id_pc_data[i]
         ax.scatter(pcs[:, 0], pcs[:, 1], color="blue", s=1.0)
         for node in points[i]:
             ax.plot(node.x, node.y, color="red", marker='x', markersize=5.0)
         plt.show()
+
+    import_training_data("./id_path_data.yaml", "./id_pc_data.yaml")
